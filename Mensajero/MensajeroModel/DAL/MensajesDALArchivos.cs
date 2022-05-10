57eaa0a46e8689c2d56ddd692fc1e80666d57f60
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-05-09 20:43:24 -0400

    Mensajero Inicial

---
+++ Mensajero/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -0,0 +1,82 @@
+﻿using MensajeroModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroModel.DAL
+{
+    public class MensajesDALArchivos : IMensajesDAL
+
+    {
+        //para implementar singleton
+        //1. El contructor tiene que ser private
+        private MensajesDALArchivos()
+        {
+
+        }
+        //2. Debe poseer un atributo del mismo tipo de la clase y estatico
+        private static MensajesDALArchivos instancia;
+        //3. Tener un metodo GetIntance, que devuelve una referencia al atributo
+        public static IMensajesDAL GetInstancia()
+        {
+            if (instancia== null)
+            {
+                instancia = new MensajesDALArchivos();
+            }
+            return instancia;
+        }
+
+
+        private static string url = Directory.GetCurrentDirectory();
+        private static string archivo = url + "/mensaje.txt";
+        public void AgregarMensaje(Mensaje mensaje)
+        {
+            try
+            {
+                using (StreamWriter write = new StreamWriter(archivo, true))
+                {
+                    write.WriteLine(mensaje.Nombre + ";" + mensaje.Texto + ";" + mensaje.Tipo);
+                    write.Flush();
+                }
+            }catch (Exception )
+            {
+
+            }
+            
+        }
+
+        public List<Mensaje> ObtenerMensajes()
+        {
+            List<Mensaje> lista = new List<Mensaje>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                {
+                    string texto = "";
+                    do
+                    {
+                        texto = reader.ReadLine();
+                        if (texto != null)
+                        {
+                            string[] arr = texto.Trim().Split(';');
+                            Mensaje mensaje = new Mensaje()
+                            {
+                                Nombre = arr[0],
+                                Texto = arr[1],
+                                Tipo = arr[2]
+                            };
+                            lista.Add(mensaje);
+                        }
+                    } while (texto != null);
+                }
+            }catch(Exception)
+            {
+                lista = null;
+            }
+            return lista;
+        }
+    }
+}
