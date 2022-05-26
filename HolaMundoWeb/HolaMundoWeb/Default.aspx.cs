260db96eae17854545fa8cd4cca81806a71f729c
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-05-26 19:19:44 -0400

    hola Mundo WEB

---
+++ HolaMundoWeb/HolaMundoWeb/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HolaMundoWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void saludarBtn_Click(object sender, EventArgs e)
+        {
+            //Para los asp components, la propiedad para obtener el valor es Text
+            string nombre = this.nombreTxt.Text.Trim();
+            //Para los HTML elemnts, la propiedad es InnerText
+            this.mensajeH1.InnerText = "Hola " + nombre + " del mundo ASP.net";
+
+        }
+    }
+}
