81a27ee8c85cf1c2f61971ec2a0526a6d9fddc82
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-05-30 20:54:51 -0400

    formulario 2

--- StarCap/StarCapWeb/Default.aspx
+++ StarCap/StarCapWeb/Default.aspx
@@ -2,6 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="Contenido" runat="server">
     <div class="row">
         <div class="col-lg-6 mx-auto">
+            <div class="mensajes">
+                <asp:Label runat="server" ID="mensajesLbl" CssClass="text-success"></asp:Label>
+
+            </div>
             <div class="card">
                 <div class="card-header bg-dark text-white">
                     <h3>Agregar Cliente</h3>
--- StarCap/StarCapWeb/Default.aspx.cs
+++ StarCap/StarCapWeb/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StarCapModel.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace StarCapWeb
 {
     public partial class Default : System.Web.UI.Page
     {
+        private IClientesDAL clientesDAL = new ClientesDALObjetos();
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,7 +18,17 @@ namespace StarCapWeb
 
         protected void agregarBtn_Click(object sender, EventArgs e)
         {
-
+            //1. Obtener los datos del formulario
+            string nombre = this.nombreTxt.Text.Trim();
+            string rut = this.rutTxt.Text.Trim();
+            //esto obtiene el valor del DropDown
+            string bebidaValor = this.bebidaDdl.SelectedValue;
+            //esto obtiene el texto
+            string bebidaTexto = this.bebidaDdl.SelectedItem.Value;
+            int nivel = Convert.ToInt32(this.nivelRbl.SelectedItem.Value);
+            //2. Construir el objeto de tipo Cliente
+            //3. Llamar al DAL
+            //4. Mostrar mensaje de exito
         }
     }
 }
