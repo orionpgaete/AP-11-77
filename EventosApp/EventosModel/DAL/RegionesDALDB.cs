97dac8239e1e1caaeb9b23773d54e3dfd6d83ebc
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-07-04 21:02:59 -0400

    EventosDB

---
+++ EventosApp/EventosModel/DAL/RegionesDALDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosModel.DAL
+{
+    public class RegionesDALDB : IRegionesDAL
+    {
+        public List<Region> ObtenerRegiones()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
